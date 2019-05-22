fa2faa6750d26ba6e5914b693b2ec476f54473b4
GAOJISHENG <GJS990825@GMAIL.COM>
2019-05-22 20:50:14 +0800

    任务整合优化

--- main.py
+++ main.py
@@ -4,47 +4,40 @@ import time
 import lcd
 from pyb import UART, Timer, LED
 
+# 数据包字节标识
+Pack_Header1 = 0
+Pack_Header2 = 1
+Pack_MainCmd = 2
+Pack_SubCmd1 = 3
+Pack_SubCmd2 = 4
+Pack_SubCmd3 = 4
+Pack_CheckSum = 6
+Pack_Ending = 7
+
+# 功能区分字节
+OpenMV_Trafficlight = 0x01
+OpenMV_ShapeRecogniction = 0x02
+
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)  # 320*240
 # sensor.set_auto_gain(True)       #设置自动增益
 # sensor.skip_frames(10)           #刷新设置
-clock = time.clock()
-lcd.init()  # Initialize the lcd screen.
-uart = UART(3, 115200, 8, None, 1)  # 创建串口对象
+# Initialize the lcd screen.
+lcd.init()
+# 创建串口对象
+uart = UART(3, 115200, 8, None, 1)
 
-data = []
 
+# LED对象创建
 LED_Red = LED(1)
 LED_Green = LED(2)
 LED_Blue = LED(3)
 
-
+# 颜色阈值设定
 red = (40, 72, 52, 81, -7, 60)
 yellow = (60, 79, -37, 35, 12, 36)
 green = (49, 84, -92, -44, -6, 57)
-
-# create a timer object using timer 4
-tim = Timer(4, freq=1)
-# tim.callback(tick)
-tim.deinit()
-
-FlagOK = 0
-show_numTab = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-num = 0
-returnData = [0x55, 0x02, 0x92, 0x02, 0x02, 0x00, 0x00, 0xBB]  # 识别失败
-runData = [0x55, 0x02, 0x92, 0x03, 0x02, 0x00, 0x00, 0xBB]  # 正在识别
-
-
-# 定时器回调函数
-def tick(timer):            # we will receive the timer object when being called
-    global FlagOK, num, returnData
-    print("Timer callback")
-    num = num-1
-    if(num == 0):
-        num = 9
-        FlagOK = 2
-        tim.deinit()
 
 
 # 串口发送函数
@@ -59,38 +52,48 @@ def Reset_List(src):
         src[i] = 0
 
 
-# 交通灯识别
-def discern_traffic_light(img):
-    for blob in img.find_blobs([red, yellow, green], roi=[0, 0, 320, 240], area_threshold=400, merge=True):
+# 获取交通灯颜色
+def Get_TrafficLightColor(img):
+    for blob in img.find_blobs([red, yellow, green], area_threshold=400, merge=True):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
-        lcd.display(img)
         return blob.code()
 
 
-#   二维码识别，并返回识别结果
-def Color_Check(srcbuf):
-    global FlagOK, num
-    if(FlagOK == 1):
-        img.draw_string(100, 180, "open"+show_numTab[num], color=[255, 0, 0])
-        for code in img.find_qrcodes():
-            FlagOK = 0
-            tim.deinit()
-            print(code)
-            qr_Tab = code.payload()
-            uart.writechar(0x55)
-            uart.writechar(0x02)
-            uart.writechar(0x92)
-            uart.writechar(0x01)
-            uart.writechar(len(qr_Tab))
-            for qrdata in qr_Tab:
-                uart.writechar(ord(qrdata))
-            uart.writechar(0xBB)
+# 交通灯识别
+def Discern_TrafficLight():
+    sensor.set_brightness(-3)
+    sensor.set_contrast(-3)
+    sensor.set_saturation(3)
+    traffic_light = 0
+    for i in range(1, 100):
+        img = sensor.snapshot()
+        traffic_light = Get_TrafficLightColor(img)
+        if(traffic_light in [1, 2, 4]):
+            break
+    sensor.skip_frames(tiem=4000)
+    sensor.set_brightness(0)
+    sensor.set_contrast(0)
+    sensor.set_saturation(0)
+    return traffic_light
 
-    if(FlagOK == 2):
-        for rdata in returnData:
-            uart.writechar(rdata)
-        FlagOK = 0
+
+# 二维码识别
+def QRCode_Recognition():
+    for i in range(10):
+        img = sensor.snapshot()
+        for code in img.find_qrcodes():
+            content = code.payload()
+            return content
+
+
+# 返回二维码识别结果
+def Return_QRCode(result):
+    length = len(result)
+    UART_Send([0x55, 0x02, 0x92, 0x01, length], 5)
+    for i in result:
+        uart.writechar(ord(i))
+    uart.writechar(0xBB)
 
 
 #  发送交通灯识别结果
@@ -106,10 +109,8 @@ def Return_TrafficLightResult(result):
         print("绿色")
     else:
         result = 3
-        print("黄色")
-
-    trafficLightReturn = [0x55, 0x02, 0x91, result, 0x00, 0x00, 0x00, 0xBB]
-    UART_Send(trafficLightReturn, 8)
+        print("默认：黄色")
+    UART_Send([0x55, 0x02, 0x91, result, 0x00, 0x00, 0x00, 0xBB], 8)
 
 
 # 正常的数据
@@ -158,62 +159,29 @@ def Check_Uart():
     return uartDataReceived
 
 
+# 主循环
 while (True):
 
     if (Check_Uart()):
         print(receiveTable)
 
+    # 二维码
+    if (receiveTable[Pack_MainCmd] == 0x92):
+        print("QRCode Recognition")
+        content = QRCode_Recognition()
+        if (content):
+            print("QRCode Content:" + content)
+            Return_QRCode(content)
+        else:
+            print("QRCode Error")
+
+    # 交通灯、图形
+    elif (receiveTable[Pack_MainCmd] == 0x91):
+        if (receiveTable[Pack_SubCmd1] == OpenMV_Trafficlight):
+            print("Traffic Light Recognition")
+            color = Discern_TrafficLight()
+            Return_TrafficLightResult(color)
+        else:
+            print("Undefined action")
     #img = sensor.snapshot()
-    # lcd.display(img)
-
-# while(True):
-
-#     img = sensor.snapshot()
-
-    # if(uart.any()):
-    #     print("接收")
-    #     data = uart.read(4)
-    #     if((data[0] == 0x55) and (data[1] == 0x02)):
-    #         if(len(data) >= 4):
-    #             for d in data:
-    #                 print(d)
-    #             print("接收成功")
-
-        # if(data[2] == 0x92):
-        #     print("识别二维码")
-        #     if(data[3] == 0x01):    #启动识别
-        #         if(FlagOK == 0):
-        #             FlagOK = 1
-        #             num = 9
-        #             print("开始识别")
-        #             tim.callback(tick)
-        #         else:
-        #             print("正在识别")
-        #             for rdata in runData:
-        #                 print(rdata)
-        #                 uart.writechar(rdata)
-        #     if(data[3] == 0x02):
-        #         print("停止识别")
-        #         FlagOK = 2
-        #         tim.deinit()       #定时器停止
-        # if(data[2] == 0x91):
-        #     print("交通灯识别")
-        #     sensor.set_brightness(-3)
-        #     sensor.set_contrast(-3)
-        #     sensor.set_saturation(3)
-        #     traffic_light = 0
-        #     for i in range(1, 100):
-        #         img = sensor.snapshot()
-        #         traffic_light = discern_traffic_light(img)
-        #         if(traffic_light in [1, 2, 4]):
-        #             break
-        #         lcd.display(img)
-        #     print(traffic_light)
-        #     sensor.skip_frames(tiem = 4000)
-        #     sensor.set_brightness(0)
-        #     sensor.set_contrast(0)
-        #     sensor.set_saturation(0)
-        #     Return_TrafficLightResult(traffic_light)
-
-    # Color_Check(data)
-    # lcd.display(img)
+    #lcd.display(img)
